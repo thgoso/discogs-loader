6f1269825ab35fd6fc271270369caf78fa86058a
thgoso <th_goso@freenet.de>
2017-12-21 21:58:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,13 @@ if [ "$EUID" -ne "0" ] ; then
   exit 1
 fi
 
-for file in "discogs_addrelease" \
-            "discogs_exportcollection" \
-            "discogs_getreldata" \
-            "discogs_getuserdata" \
-            "discogs_writecustom" ; do
+for file in "discogs_exportcollection" \
+            "discogs_importcollection" \
+            "discogs_getreldata" ; do
 
   echo "install: /usr/local/bin/$file"
   cp "$file" "/usr/local/bin/$file"
   chmod 755 "/usr/local/bin/$file"
 done
-
-echo "Now you can delete the src-folder"
 
 exit 0
